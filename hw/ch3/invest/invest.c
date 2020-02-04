31e6c66d7c70e2abb4ee64cfd99f9680029aef25
alex hay <alexanderhay2020@u.northwestern.edu>
2020-02-03 23:51:41 -0600

    commented what was added and changed

--- hw/ch3/invest/invest.c
+++ hw/ch3/invest/invest.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "NU32.h"          // constants, funcs for startup and UART
+#include "NU32.h"                                                  // added
 #define MAX_YEARS 100
 #define MAX_MESSAGE_LENGTH 20
 
@@ -14,16 +14,17 @@ typedef struct {
 
 int getUserInput(Investment *invp){
   int valid;
-  char message[MAX_MESSAGE_LENGTH];
+  char message[MAX_MESSAGE_LENGTH];   // added
 
-  NU32_WriteUART3("Enter investment, growth rate, number of years");
-  NU32_WriteUART3("\r\n");
-  NU32_ReadUART3(message, MAX_MESSAGE_LENGTH);
+  NU32_WriteUART3("Enter investment, growth rate, number of years");    // changed from printf
+  NU32_WriteUART3("\r\n");                                              // added
+  NU32_ReadUART3(message, MAX_MESSAGE_LENGTH);                          // added
   sscanf(message, "%lf %lf %d", &(invp->inv0), &(invp->growth), &(invp->years));
-  NU32_WriteUART3(message);                     // send message back
-  NU32_WriteUART3("\r\n");                      // carriage return and newline
+  NU32_WriteUART3(message);                                             // added
+  NU32_WriteUART3("\r\n");                                              // added
   valid = (invp->inv0 > 0) && (invp->growth > 0) && (invp->years > 0) && (invp->years <= MAX_YEARS);
 
+  // this is being weird
   // NU32_WriteUART3("Valid input?");
   // NU32_WriteUART3(valid);
   return(valid);
@@ -43,21 +44,21 @@ void sendOutput(double *arr, int yrs){
   int i;
   char outstring[100];
 
-  NU32_WriteUART3("\r\nRESULTS:\r\n");
+  NU32_WriteUART3("\r\nRESULTS:\r\n");                                  // changed; added return carriage tag
 
   for(i=0; i<=yrs;i++){
     sprintf(outstring, "Year %3d:   %10.2f\r\n", i, arr[i]);
 
-    NU32_WriteUART3(outstring);
+    NU32_WriteUART3(outstring);                                         // changed from printf
   }
 
-  NU32_WriteUART3("\r\n");
+  NU32_WriteUART3("\r\n");                                              // added
 }
 
 
 int main(void){
   Investment inv;
-  NU32_Startup(); // cache on, interrupts on, LED/button init, UART init
+  NU32_Startup();                                                       // added
 
   while(getUserInput(&inv)){
     inv.invarray[0] = inv.inv0;
