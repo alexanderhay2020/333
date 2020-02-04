3aee3cb571c611ef8e821d78b8f2c8f4aea77ebe
alex hay <alexanderhay2020@u.northwestern.edu>
2020-02-03 23:20:32 -0600

    invest.c working as it should validation check removed for time being

--- hw/ch3/invest/invest.c
+++ hw/ch3/invest/invest.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "NU32.h"          // constants, funcs for startup and UART
 #define MAX_YEARS 100
-#define MAX_MESSAGE_LENGTH 3
+#define MAX_MESSAGE_LENGTH 20
 
 
 typedef struct {
@@ -12,30 +12,59 @@ typedef struct {
 } Investment;
 
 
-int main(void){
-  Investment inv;
-  NU32_Startup(); // cache on, interrupts on, LED/button init, UART init
-
-  while(getUserInput(&inv)){
-    NU32_WriteUART3("Enter investment, growth rate, number of years");
-    NU32_WriteUART3("\r\n");                      // carriage return and newline
-
-  }
-
-  return 0;
-}
-
-
 int getUserInput(Investment *invp){
   int valid;
   char message[MAX_MESSAGE_LENGTH];
 
   NU32_WriteUART3("Enter investment, growth rate, number of years");
-
+  NU32_WriteUART3("\r\n");
   NU32_ReadUART3(message, MAX_MESSAGE_LENGTH);
   sscanf(message, "%lf %lf %d", &(invp->inv0), &(invp->growth), &(invp->years));
-
+  NU32_WriteUART3(message);                     // send message back
+  NU32_WriteUART3("\r\n");                      // carriage return and newline
   valid = (invp->inv0 > 0) && (invp->growth > 0) && (invp->years > 0) && (invp->years <= MAX_YEARS);
 
+  // NU32_WriteUART3("Valid input?");
+  // NU32_WriteUART3(valid);
   return(valid);
+}
+
+
+void calculateGrowth(Investment *invp){
+  int i;
+
+  for(i=1; i<= invp->years; i=i+1){
+    invp->invarray[i] = invp->growth * invp->invarray[i-1];
+  }
+}
+
+
+void sendOutput(double *arr, int yrs){
+  int i;
+  char outstring[100];
+
+  NU32_WriteUART3("\r\nRESULTS:\r\n");
+
+  for(i=0; i<=yrs;i++){
+    sprintf(outstring, "Year %3d:   %10.2f\r\n", i, arr[i]);
+
+    NU32_WriteUART3(outstring);
+  }
+
+  NU32_WriteUART3("\r\n");
+}
+
+
+int main(void){
+  Investment inv;
+  NU32_Startup(); // cache on, interrupts on, LED/button init, UART init
+
+  while(getUserInput(&inv)){
+    inv.invarray[0] = inv.inv0;
+    calculateGrowth(&inv);
+    sendOutput(inv.invarray, inv.years);
+
+  }
+
+  return 0;
 }
