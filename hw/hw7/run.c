493477c75d612203f99f543fff6ece78cdd4f22d
alex hay <alexanderhay2020@u.northwestern.edu>
2020-02-25 17:03:13 -0600

    hw7 submitted

--- hw/hw7/run.c
+++ hw/hw7/run.c
@@ -5,9 +5,10 @@ static volatile int Waveform[NUMSAMPS]; // waveform
 
 
 void makeWaveform(){
+
   int i = 0;
-  center = ?;                     // Center
-  A = ?;                          // Amplitude
+  int center = 2000;                     // Center based on PR3 (1/2 PR3)
+  int A = 1000;                          // Amplitude (3) 25% of PR3
 
   for (i=0; i<NUMSAMPS; i++){
     if (i<NUMSAMPS/2){
@@ -20,10 +21,26 @@ void makeWaveform(){
 }
 
 
+void __ISR(_TIMER_2_VECTOR, IPL5SOFT) Controller(void) { // _TIMER_2_VECTOR = 8
+
+  static int counter = 0;         // initialize counter once
+  OC1RS = Waveform[counter];      // duty cycle = OC1RS/(PR2+1) = 25%
+  counter++;                      // add one to counter every time ISR is entered
+
+  if (counter == NUMSAMPS) {
+    counter = 0;                  // roll the counter over when needed
+  }
+  IFS0CLR = 0x1000;               // clear interrupt flag IFS0
+}
+
+
 int main(void){
+
   NU32_Startup();                 // cache on, interrupts on, LED/button init, UART init
 
   TRISDbits.TRISD0 = 0;           // make D1 an output
+
+  makeWaveform();
 
   T3CONbits.TCKPS = 0;            // Timer3 prescaler N=1
   PR3 = 3999;                     // period = (PR3+1) * N * 12.5 ns = 50us. 20kHz
@@ -34,6 +51,13 @@ int main(void){
   OC1R = 3000;                    // initialize before turning OC1 on; afterward it is read-only
   T3CONbits.ON = 1;               // turn on Timer3
   OC1CONbits.ON = 1;              // turn on OC1
+
+  __builtin_disable_interrupts(); // step 2: disable interrupts
+  INTCONbits.INT0EP = 0;          // step 3: INT0 triggers on falling edge
+  // IPC3bits.T3IP = 5;              // step 4: interrupt priority 5
+  IFS0bits.INT0IF = 0;            // step 5: clear the int flag
+  IEC0bits.INT0IE = 1;            // step 6: enable INT0 by setting IEC0<3>
+  __builtin_enable_interrupts();  // step 7: enable interrupts
 
   while(1){
     ;
